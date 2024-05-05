57f4c0920549d559cb7b6cf1746fb6d66521071d
vicok2003 <vicok2003@gmail.com>
2024-05-05 20:19:24 +0100

    added links and fixed display issue

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import './ComingSoon.css'; 
+import '../styles/ComingSoon.css'; 
 import logo from '../Img_assets/Beyond-Borders-logo-Teal.svg'; 
-import facebookIcon from '../Img_assets/logo Vector 2.png'; 
-import twitterIcon from '../Img_assets/logo vector.png'; 
-import instagramIcon from '../Img_assets/Instagram logo.png'; 
-import linkedinIcon from '../Img_assets/linkedin logo.png'; 
-import youtubeIcon from '../Img_assets/Subtract.png'; 
+import linkedinIcon from '../Img_assets/linkedin logo.svg'; 
+import instagramIcon from '../Img_assets/Instagram logo.svg'; 
+import linkTree from '../Img_assets/linktree.svg';
 import leftBoxImage from '../Img_assets/Block01.png' ;
 import rightBoxImage from '../Img_assets/Block02.png';
 import longBlock from '../Img_assets/Block03.png';
@@ -16,7 +14,9 @@ export default function ComingSoon() {
     <div className="coming-soon-container">
       <div className="top-header">
       <img src={logo} alt="Logo" className="logo" />
-      <button className="notify-me-btn">Notify Me</button>
+      <a href="https://subscribepage.io/beyondbordersletters" target="_blank" rel="noopener noreferrer">
+        <button className="notify-me-btn">Notify Me</button>  
+      </a>
       </div>
 
       <div className="comingsoon-message">
@@ -24,20 +24,14 @@ export default function ComingSoon() {
         <p className="paragraph">Our mission is to equip individuals with the skills, resources, mentorship, and community that they need to achieve their career and educational goals.</p>
         
         <div className="social-icons-container">
-        <a href="#">
-            <img src={facebookIcon} alt="Facebook" />
-          </a>
-          <a href="#">
-            <img src={twitterIcon} alt="Twitter" />
-          </a>
-          <a href="#">
-            <img src={youtubeIcon} alt="Instagram" />
-          </a>
-          <a href="#">
+          <a href="https://www.linkedin.com/company/beyond-borders-hq/" target="_blank" rel="noopener noreferrer">
             <img src={linkedinIcon} alt="LinkedIn" />
           </a>
-          <a href="#">
-            <img src={instagramIcon} alt="YouTube" />
+          <a href="https://www.instagram.com/beyondbordershq/" target="_blank" rel="noopener noreferrer">
+            <img src={instagramIcon} alt="Instagram" />
+          </a>
+          <a href="https://linktr.ee/beyondbordershq" target="_blank" rel="noopener noreferrer">
+            <img src={linkTree} alt="linkTree" />
           </a>
         </div>
       </div>
